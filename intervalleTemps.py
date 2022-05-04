7a462e64882fc2394298c80b08c65deca6d0f12d
Florian <escande.florian.chatco@gmail.com>
2022-05-04 18:07:22 +0200

    script table trajet fonctionnel à l'exeption des é

--- intervalleTemps.py
+++ intervalleTemps.py
@@ -13,39 +13,44 @@ df_arret = df_arret.dropna()
 #convert all df_arret values into string
 df_arret = df_arret.astype(str)
 
+arreta12 = df_arret.astype(str)['ArretA12']
+arretb12 = df_arret.astype(str)['ArretB12']
+arreta13 = df_arret.astype(str)['ArretA13']
+arretb13 = df_arret.astype(str)['ArretB13']
+arreta14 = df_arret.astype(str)['ArretA14']
+arretb14 = df_arret.astype(str)['ArretB14']
+arreta15 = df_arret.astype(str)['ArretA15']
+arretb15 = df_arret.astype(str)['ArretB15']
+
 
 final_file = open('intervalleTemps.txt', 'w')
 
-#le truc ci-dessous marche
-# for i in range(0, len(df_arret['ArretA12'])):
-#     final_file.write(df_arret['ArretA12'][i] + ' ' + '\n')
-
-with open('arret.txt') as arret_file:
+with open('doublon.txt') as arret_file:
     for num, line in enumerate(arret_file, 1):
         for i in range(0, len(df_arret['ArretA12'])):
             if str(df_arret['ArretA12'][i]) in line:
-                final_file.write(df_arret['ArretA12'][i] + ' ' + str(num) + '\n')
+                arreta12[i]=str(num)
         for i in range(0, len(df_arret['ArretB12'])):
             if str(df_arret['ArretB12'][i]) in line:
-                final_file.write(df_arret['ArretB12'][i] + ' ' + str(num) + '\n')
+                arretb12[i]=str(num)
         for i in range(0, len(df_arret['ArretA13'])):
             if str(df_arret['ArretA13'][i]) in line:
-                final_file.write(df_arret['ArretA13'][i] + ' ' + str(num) + '\n')
+                arreta13[i]=str(num)
         for i in range(0, len(df_arret['ArretB13'])):
             if str(df_arret['ArretB13'][i]) in line:
-                final_file.write(df_arret['ArretB13'][i] + ' ' + str(num) + '\n')
+                arretb13[i]=str(num)
         for i in range(0, len(df_arret['ArretA14'])):
             if str(df_arret['ArretA14'][i]) in line:
-                final_file.write(df_arret['ArretA14'][i] + ' ' + str(num) + '\n')
+                arreta14[i]=str(num)
         for i in range(0, len(df_arret['ArretB14'])):
             if str(df_arret['ArretB14'][i]) in line:
-                final_file.write(df_arret['ArretB14'][i] + ' ' + str(num) + '\n')
+                arretb14[i]=str(num)
         for i in range(0, len(df_arret['ArretA15'])):
             if str(df_arret['ArretA15'][i]) in line:
-                final_file.write(df_arret['ArretA15'][i] + ' ' + str(num) + '\n')
+                arreta15[i]=str(num)
         for i in range(0, len(df_arret['ArretB15'])):
             if str(df_arret['ArretB15'][i]) in line:
-                final_file.write(df_arret['ArretB15'][i] + ' ' + str(num) + '\n')
+                arretb15[i]=str(num)
                 
         # final_file.write(line)
         # final_file.write(str(num))
@@ -55,9 +60,19 @@ with open('arret.txt') as arret_file:
 # for index, row in df_arret.iterrows():
 #     final_file.write(row['ArretA12'])
 
-
-final_file.write("eyow")
 arret_file.close()
+final_file.close()
+
+final_file = open('intervalleTemps.txt','w')
+
+for index, row in df_arret.iterrows():
+    final_file.write(patterns + '(' + arreta12[index] + ',' + arretb12[index] + ',' + str(row['Temps12']) + ');' + "\n")
+    final_file.write(patterns + '(' + arreta13[index] + ',' + arretb13[index] + ',' + str(row['Temps13']) + ');' + "\n")
+    final_file.write(patterns + '(' + arreta14[index] + ',' + arretb14[index] + ',' + str(row['Temps14']) + ');' + "\n")
+    final_file.write(patterns + '(' + arreta15[index] + ',' + arretb15[index] + ',' + str(row['Temps15']) + ');' + "\n")
+
+
+
 final_file.close()
 
 # #remove the (arreta, arretb, temps) lines where there is a dublon of arreta and arretb combination
@@ -65,12 +80,3 @@ final_file.close()
 
 
 #final_file = open('intervalleTemps.txt','w')
-
-#write df arret in intervalleTemps.txt
-# for index, row in df_arret.iterrows():
-#     final_file.write(patterns + '(' + str(row['ArretA12']) + ',' + str(row['ArretB12']) + ',' + str(int(row['Temps12'])) + ');' + "\n")
-#     final_file.write(patterns + '(' + str(row['ArretA13']) + ',' + str(row['ArretB13']) + ',' + str(int(row['Temps13'])) + ');' + "\n")
-#     final_file.write(patterns + '(' + str(row['ArretA14']) + ',' + str(row['ArretB14']) + ',' + str(int(row['Temps14'])) + ');' + "\n")
-#     final_file.write(patterns + '(' + str(row['ArretA15']) + ',' + str(row['ArretB15']) + ',' + str(int(row['Temps15'])) + ');' + "\n")
-
-# final_file.close()
